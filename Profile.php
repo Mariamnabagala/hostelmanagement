7da7666f31fab096087630cc6de8936903b41467
MariamNabagala <146656718+Mariamnabagala@users.noreply.github.com>
2024-03-29 09:05:14 +0300

    Add files via upload

--- Profile.php
+++ Profile.php
@@ -28,6 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href ="booking.css">
 
 </head>
 <body>
